ab4810ad44a352961f8955186d6d156c8925e38f
Steven Duval Ruilova <sduval215@gmail.com>
2021-02-16 21:27:12 -0800

    implement main upload ui styles and logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 
+import Main from './components/Main';
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <Main/>
     );
   }
 }
