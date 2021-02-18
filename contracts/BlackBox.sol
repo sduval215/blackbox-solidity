d9f9556c2b88b6d584692f17654a1837c5dc82e0
Steven Duval Ruilova <sduval215@gmail.com>
2021-02-17 20:04:36 -0800

    implement base blackbox smart contract data structures and put/query logic

--- contracts/BlackBox.sol
+++ contracts/BlackBox.sol
@@ -1,11 +1,52 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.21 <0.7.0;
+pragma experimental ABIEncoderV2;
 
+/**
+  @title A IPFS hashtag data mapper
+  @author Steven Duval Ruilova
+  @notice Still very much Proof of Concept
+  @dev Roughtly version 0.0.1
+ */
 contract BlackBox {
   address public owner;
-  string public name = "BlackBox DApp";
+  string public name = "BlackBox DApp Smart Contract";
+
+  struct Submission {
+    string pathHash;
+    uint256 published;
+  }
+
+  struct User {
+    Submission[] submissions;
+  }
+
+  mapping(address => User) users;
 
   constructor() public {
     owner = msg.sender;
+  }
+
+  /**
+    @dev handles submission array data returns based
+    on msg.sender value
+  */
+  function getUserSubmissions () public view returns (Submission[] memory submissions) {
+    return(users[msg.sender].submissions);
+  }
+
+  /**
+    @dev handles hashPath data saving onto specific mapping
+    based on msg.sender value
+  */
+  function publishFile (string memory hashPath) public returns (bool success) {
+    users[msg.sender].submissions.push(
+      Submission({
+        pathHash: hashPath,
+        published: now
+      })
+    );
+
+    return true;
   }
 }
